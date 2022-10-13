f3274a22153369b89f15e673e21588493405f0e8
jacky wang <jackyw.se@gmail.com>
2022-10-13 16:05:44 -0700

    updated deployed contract on testnet

--- deployed.ts
+++ deployed.ts
@@ -1,5 +1,5 @@
 export const DEPLOYED = {
-  "testnet": "0x5890f9fea5d4c9e54724f423efb89655329db2525268e690258deaa8557f5e3c",
+  "testnet": "0xfa25d54478b893f1723b0c823ad4b49a72d0a441f941c744e72d495aae013c85",
   "devnet": "0xfa25d54478b893f1723b0c823ad4b49a72d0a441f941c744e72d495aae013c85",
   "custom": "_"
 };
