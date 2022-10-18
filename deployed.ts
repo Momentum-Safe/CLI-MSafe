3645c731d3a39c6999b1d743b2ae8053e29ece3c
jacky wang <jackyw.se@gmail.com>
2022-10-18 16:35:01 -0700

    updated mainnet address after mainnet deployment

--- deployed.ts
+++ deployed.ts
@@ -1,6 +1,6 @@
 export const DEPLOYED = new Map<string, string>([
   ["testnet", "0x74f14286e43d27ed0acc0c4548a5be99a7c2af3cf17a1344c87b7f026b2fcc23"],
   ["devnet", "0x74f14286e43d27ed0acc0c4548a5be99a7c2af3cf17a1344c87b7f026b2fcc23"],
-  ["mainnet", "_"],
+  ["mainnet", "0xaa90e0d9d16b63ba4a289fb0dc8d1b454058b21c9b5c76864f825d5c1f32582e"],
   ["custom", "_"],
 ]);
