b7e4a754735c4a17d6fdd5ab8c06f49b86e38048
Wesley Graham <wgraham@berkeley.edu>
2023-04-04 09:32:21 -0700

    feat(tha-1020): initialize collaterals for protocol launch

--- scripts/admin/entryPayloads.ts
+++ scripts/admin/entryPayloads.ts
@@ -1,16 +1,21 @@
-import {BCS, TxnBuilderTypes} from "aptos";
+import {BCS, HexString, TxnBuilderTypes} from "aptos";
 import {EntryFunctionArgs} from "../../src/momentum-safe/msafe-txn";
 
-// Types
-type MinLiabilityAmountArgs = {
-    amount: number
-}
-
+// Vault Types
 type InitializeCollateralArgs = {
     mcr_bps: number,
     mint_cap: number
 }
 
+type MintCapArgs = {
+    cap: number,
+}
+
+type MinLiabilityAmountArgs = {
+    amount: number
+}
+
+// Oracle Types
 type ConfigurePythArgs = {
     new_feed_id: string
 }
@@ -19,11 +24,17 @@ type ConfigureTier1OracleArgs = {
     new_oracle: number
 }
 
+type ConfigureSwitchboardArgs = {
+    new_aggregator_address: string,
+}
+
+// THL Types
 type MintTHLArgs = {
     recipient: string,
     amount: number
 }
 
+// LBP Types
 type CreateLBPArgs = {
     asset_0_amount: number,
     asset_1_amount: number,
@@ -39,6 +50,14 @@ type GrantWhitelistArgs = {
 }
 
 // Vault Operation Data
+export function initializeCollateralOperationData(typeArgs: string[], args: InitializeCollateralArgs): EntryFunctionArgs {
+    return {
+        fnName: "0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::init::initialize_collateral",
+        typeArgs,
+        args: [BCS.bcsSerializeUint64(args.mcr_bps), BCS.bcsSerializeUint64(args.mint_cap)]
+    };
+}
+
 export function setMinLiabilityAmountOperationData(args: MinLiabilityAmountArgs): EntryFunctionArgs {
     return {
         fnName: "0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::vault::set_min_liability_amount",
@@ -47,29 +66,36 @@ export function setMinLiabilityAmountOperationData(args: MinLiabilityAmountArgs)
     };
 }
 
-export function initializeCollateralOperationData(typeArgs: string[], args: InitializeCollateralArgs): EntryFunctionArgs {
+export function setMintCapOperationData(typeArgs: string[], args: MintCapArgs): EntryFunctionArgs {
     return {
-        fnName: "0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::init::initialize_collateral",
-        typeArgs: [typeArgs[0]],
-        args: [BCS.bcsSerializeUint64(args.mcr_bps), BCS.bcsSerializeUint64(args.mint_cap)]
+        fnName: "0x6f986d146e4a90b828d8c12c14b6f4e003fdff11a8eecceceb63744363eaac01::vault::set_mint_cap",
+        typeArgs,
+        args: [BCS.bcsSerializeUint64(args.cap)] // amount
     };
 }
 
 // Oracle Operation Data
 export function configurePythOperationData(typeArgs: string[], args: ConfigurePythArgs): EntryFunctionArgs {
     return {
-        fnName: "0x92e95ed77b5ac815d3fbc2227e76db238339e9ca43ace45031ec2589bea5b8c::pyth_oracle::configure_pyth",
-        typeArgs: [typeArgs[0]],
-        // TODO: HANDLE Vector<u8> inputs
-        args: [BCS.bcsToBytes(TxnBuilderTypes.AccountAddress.fromHex(args.new_feed_id))]
+        fnName: "0x092e95ed77b5ac815d3fbc2227e76db238339e9ca43ace45031ec2589bea5b8c::pyth_oracle::configure_pyth",
+        typeArgs,
+        args: [BCS.bcsSerializeBytes(new HexString(args.new_feed_id).toUint8Array())]
     };
 }
 
 export function configureTier1OracleOperationData(typeArgs: string[], args: ConfigureTier1OracleArgs): EntryFunctionArgs {
     return {
-        fnName: "0x92e95ed77b5ac815d3fbc2227e76db238339e9ca43ace45031ec2589bea5b8c::tiered_oracle::configure_tier_1",
-        typeArgs: [typeArgs[0]],
-        args: [BCS.bcsSerializeUint64(args.new_oracle)]
+        fnName: "0x092e95ed77b5ac815d3fbc2227e76db238339e9ca43ace45031ec2589bea5b8c::tiered_oracle::configure_tier_1",
+        typeArgs,
+        args: [BCS.bcsSerializeU8(args.new_oracle)]
+    };
+}
+
+export function configureSwitchboardOperationData(typeArgs: string[], args: ConfigureSwitchboardArgs): EntryFunctionArgs {
+    return {
+        fnName: "0x092e95ed77b5ac815d3fbc2227e76db238339e9ca43ace45031ec2589bea5b8c::switchboard_oracle::configure_switchboard",
+        typeArgs,
+        args: [BCS.bcsToBytes(TxnBuilderTypes.AccountAddress.fromHex(args.new_aggregator_address))]
     };
 }
 
@@ -89,10 +115,7 @@ export function mintTHLOperationData(args: MintTHLArgs): EntryFunctionArgs {
 export function createLBPOperationData(typeArgs: string[], args: CreateLBPArgs): EntryFunctionArgs {
     return {
         fnName: "0x6970b4878c3aea96732be3f31c2dded12d94d9455ff0c76c67d84859dce35136::lbp_scripts::create_lbp",
-        typeArgs: [
-            typeArgs[0],
-            typeArgs[1],
-        ],
+        typeArgs,
         args: [
             BCS.bcsSerializeUint64(args.asset_0_amount),
             BCS.bcsSerializeUint64(args.asset_1_amount),
@@ -108,10 +131,7 @@ export function createLBPOperationData(typeArgs: string[], args: CreateLBPArgs):
 export function grantWhitelistOperationData(typeArgs: string[], args: GrantWhitelistArgs): EntryFunctionArgs {
     return {
         fnName: "0x6970b4878c3aea96732be3f31c2dded12d94d9455ff0c76c67d84859dce35136::lbp::grant_whitelist",
-        typeArgs: [
-            typeArgs[0],
-            typeArgs[1],
-        ],
+        typeArgs,
         args: [
             BCS.bcsToBytes(TxnBuilderTypes.AccountAddress.fromHex(args.creator_address))
         ]
